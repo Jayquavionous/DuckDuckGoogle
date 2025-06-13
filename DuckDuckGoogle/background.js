fd8a141bf4d598f2e890b4e26b7be9020bb0d3ae
J <76923701+Jayquavionous@users.noreply.github.com>
2025-06-13 03:44:49 +0000

    nvm shortening query doesnt look good i wanna match with the q in the actual url

--- DuckDuckGoogle/background.js
+++ DuckDuckGoogle/background.js
@@ -2,10 +2,10 @@ browser.browserAction.onClicked.addListener((tab) => {
   if (!tab.url.includes("duckduckgo.com/")) return;
 
   const url = new URL(tab.url);
-  const q = url.searchParams.get("q");
+  const query = url.searchParams.get("q");
 
-  if (q) {
-    const google = "https://www.google.com/search?q=" + encodeURIComponent(q);
+  if (query) {
+    const google = "https://www.google.com/search?q=" + encodeURIComponent(query);
     browser.tabs.update(tab.id, { url: google });
   }
 });
